be0cf8314bfa2a8619027f94b6e0bb0fee95a7d2
Brayteh <ya3koub5@gmail.com>
2025-07-18 11:57:36 +0200

    aufgabe 2

--- auf_4_5_3_custom/lib/main.dart
+++ auf_4_5_3_custom/lib/main.dart
@@ -1,20 +1,40 @@
-import 'package:auf_4_5_3_custom/travel_home_page.dart';
 import 'package:flutter/material.dart';
+import 'travel_home_page.dart';
 
 void main() {
   runApp(const TravelApp());
 }
 
-class TravelApp extends StatelessWidget {
+class TravelApp extends StatefulWidget {
   const TravelApp({super.key});
+
+  @override
+  State<TravelApp> createState() => _TravelAppState();
+}
+
+class _TravelAppState extends State<TravelApp> {
+  int _currentPageIndex = 0;
+
+  // الصفحات التي سيتم التبديل بينها
+  final List<Widget> _pages = const [
+    TravelHomePage(),
+    Center(child: Text("🔍 Suche Seite", style: TextStyle(fontSize: 24))),
+    Center(child: Text("👤 Profil Seite", style: TextStyle(fontSize: 24))),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Travel Picks")),
-        body: const TravelHomePage(),
+        body: _pages[_currentPageIndex], // نعرض الصفحة المختارة هنا
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentPageIndex, // ضروري لتحديث الواجهة
+          onTap: (int index) {
+            setState(() {
+              _currentPageIndex = index;
+            });
+          },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: "Suche"),
@@ -25,6 +45,3 @@ class TravelApp extends StatelessWidget {
     );
   }
 }
-
-
-
