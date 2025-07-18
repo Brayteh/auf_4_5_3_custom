fdad11584fc5f3dbf997a549a26b97a3a6bed48f
Brayteh <ya3koub5@gmail.com>
2025-07-18 11:47:46 +0200

    aufgabe 1

---
+++ auf_4_5_3_custom/android/app/src/main/kotlin/com/example/auf_4_5_3_custom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.auf_4_5_3_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ auf_4_5_3_custom/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:auf_4_5_3_custom/travel_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Travel Picks")),
+        body: const TravelHomePage(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Suche"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
